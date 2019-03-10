9bb6a94cf636c1dc7aa3cc8a25d9c64e0b77a270
Vijender Singh <31817007+vijender412@users.noreply.github.com>
2019-03-10 18:59:00 +0530

    Add files via upload

---
+++ cat_dog_cnn.py
@@ -0,0 +1,92 @@
+# This is an example of loading your own data to the model. This model is classifiying cats vs dogs on the data saved on the disk using keras.
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.optimizers import SGD
+from keras.layers import Dense
+from keras.utils import np_utils
+from imutils import paths
+import numpy as np
+import argparse
+import cv2
+import os
+
+
+
+def image_to_feature_vector(image, size=(32, 32)):
+# resize the image to a fixed size, then flatten the image into
+# a list of raw pixel
+    return cv2.resize(image, size, interpolation = cv2.INTER_LINEAR).flatten()
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True,
+help="path to input dataset")
+args = vars(ap.parse_args())
+
+
+# grab the list of images that we'll be describing
+print("[INFO] describing images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# initialize the data matrix and labels list
+data = []
+labels = []
+
+path = r"C:/Users/ignite111/Desktop/new_ai/cats/"
+for imagePath in os.listdir(path):
+    image = cv2.imread(path+imagePath)
+    #   print(image)
+    label = imagePath.split(os.path.sep)[-1].split(".")[0]
+    print(label)
+    print(image)
+    features = image_to_feature_vector(image)
+    data.append(features)
+    labels.append(label)
+
+# encode the labels, converting them from strings to integers
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+# scale the input image pixels to the range [0, 1], then transform
+# the labels into vectors in the range [0, num_classes] -- this
+# generates a vector for each label where the index of the label
+# is set to `1` and all other entries to `0`
+
+data = np.array(data) / 255.0
+labels = np_utils.to_categorical(labels, 2)
+
+# partition the data into training and testing splits, using 75%
+# of the data for training and the remaining 25% for testing
+
+print("[INFO] constructing training/testing split...")
+(trainData, testData, trainLabels, testLabels) = train_test_split(
+data, labels, test_size=0.25, random_state=42)
+
+# define the architecture of the network
+model = Sequential()
+model.add(Dense(768, input_dim=3072, init="uniform",
+activation="relu"))
+model.add(Dense(384, init="uniform", activation="relu"))
+model.add(Dense(2))
+model.add(Activation("softmax"))
+
+# train the model using SGD
+print("[INFO] compiling model...")
+sgd = SGD(lr=0.01)
+model.compile(loss="binary_crossentropy", optimizer=sgd,
+metrics=["accuracy"])
+model.fit(trainData, trainLabels, nb_epoch=50, batch_size=128,
+verbose=1)
+
+# show the accuracy on the testing set
+print("[INFO] evaluating on testing set...")
+(loss, accuracy) = model.evaluate(testData, testLabels,
+batch_size=128, verbose=1)
+print("[INFO] loss={:.4f}, accuracy: {:.4f}%".format(loss,
+accuracy * 100))
+
+model.save('my_model.h5')
+model.save_weights('my_model_weights.h5')
